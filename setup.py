9d6caeeb9a39f46b40c916fd6cb89805403df2c3
Daniel Saromo Mori <danielsaromo@gmail.com>
2020-04-24 19:34:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyduino_bridge",
+    name="pyduinobridge",
     version="1.0.0",
     author="Daniel Saromo Mori",
     author_email="danielsaromo@gmail.com",
     description="A transparent library for communication between Python and Arduino",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/DanielSaromo/PyDuino_Bridge",
+    url="https://github.com/DanielSaromo/PyDuinoBridge",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
