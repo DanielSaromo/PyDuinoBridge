5efe7c848360145d8c434c5119f6214c41859737
Daniel Saromo Mori <danielsaromo@gmail.com>
2020-04-25 17:22:22 -0500

    Update pyduino_bridge.cpp

--- src/pyduino_bridge.cpp
+++ src/pyduino_bridge.cpp
@@ -8,9 +8,23 @@
 
   The code is as minimalist as possible, so that you can keep your
   existing python and arduino and use it with the library.
-  Python code tested in: Windows. To test: Jetson Nano, Raspberry Pi and Linux. 
+  Python code tested in: Windows, Jetson Nano and Raspberry Pi. 
   Created by Daniel Saromo, Apr 23, 2020.
   Released into the public domain.
+
+  Configuration considerations:
+
+  By default, the maximum number of characters allowed in the messages between Arduino and Python is 40.
+  If you want to change this buffer size, you have to put the same value in both the Python (change the value `buffSize`
+  when using the `begin` method) and the Arduino code (change the line `#define buffSize 40` in the `pyduino_bridge.h` and
+  `pyduino_bridge.cpp` files).
+
+  Also, verify that the same `numIntValues_FromPy` and `numFloatsValues_FromPy` values are configured in the Python code
+  you are using, and also in the Arduino library files (before uploading, change the lines `#define numIntValues_FromPy 1`
+  and `#define numFloatValues_FromPy 1` in the `pyduino_bridge.h` and `pyduino_bridge.cpp`). In the Arduino library files,
+  this number is set by the `#define` command, since the value representing the number of elements in the array must be a
+  constant expression, because arrays are blocks of static memory whose size must be determined at the compilation time,
+  before the program runs (for more information, see http://www.cplusplus.com/doc/tutorial/arrays/).
 
   Adapted from Robin2 code: https://forum.arduino.cc/index.php?topic=225329.msg1810764#msg1810764
   Robin2 modification for Python3: https://forum.arduino.cc/index.php?topic=566897.0
