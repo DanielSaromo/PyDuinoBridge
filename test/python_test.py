5218f6ac9dfbbae335fc44005d50dc26047eb89c
Daniel Saromo Mori <39652005+DanielSaromo@users.noreply.github.com>
2020-04-24 23:22:27 -0500

    Update python_test.py

--- test/python_test.py
+++ test/python_test.py
@@ -1,7 +1,7 @@
-# python_example.py - PyDuino Bridge: Library for transparent bidirectional
+# python_test.py - PyDuino Bridge: Library for transparent bidirectional
 # communication between Python and Arduino. Python Example Code.
 ###############################################################################################################
-# Author: Daniel Saromo. Adapted from Robin2 code.
+# Author: Daniel Saromo.
 ###############################################################################################################
 # Description:
 #
